a3774bc6fb9900721d7067a0d6e83164adf0f780
Ari Sosnovsky <Ariel@Sosnovsky.ca>
2021-11-25 14:05:44 -0500

    add icon

--- webapp/src/components/Icons/index.stories.tsx
+++ webapp/src/components/Icons/index.stories.tsx
@@ -1,22 +1,24 @@
 import { ComponentStory } from "@storybook/react";
 import { usePageTheme } from "styles/page";
 import { IconBase } from "./base";
-import {PencilIcon, CheckMarkIcon} from ".";
+import { PencilIcon, CheckMarkIcon } from ".";
+import { AddIcon } from './index';
 
 export default {
-    title: 'Icons',
-    component: IconBase,
+  title: 'Icons',
+  component: IconBase,
 };
-  
+
 
 export const IconList: ComponentStory<typeof IconBase> = (args) => {
-    const {classes} = usePageTheme({});
-    return <div className={classes.page}>
-        <PencilIcon {...args}/>
-        <CheckMarkIcon {...args}/>
-    </div> 
+  const { classes } = usePageTheme({});
+  return <div className={classes.page}>
+    <PencilIcon {...args} />
+    <CheckMarkIcon {...args} />
+    <AddIcon {...args} />
+  </div>
 }
 IconList.args = {
-    size: 2,
-    color: 'red'
+  size: 2,
+  color: 'red'
 }
