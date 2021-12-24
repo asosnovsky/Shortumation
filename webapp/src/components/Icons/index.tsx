ca48891597a1698418b7baf0057d1e70bf19f9e9
Ari Sosnovsky <Ariel@Sosnovsky.ca>
2021-12-24 11:10:06 -0500

    added arrow icon

--- webapp/src/components/Icons/index.tsx
+++ webapp/src/components/Icons/index.tsx
@@ -47,3 +47,10 @@ export const InfoIcon: FC<IconBaseProps> = props => <IconBase {...props}
 >
   <path xmlns="http://www.w3.org/2000/svg" d="M12 0c6.623 0 12 5.377 12 12s-5.377 12-12 12-12-5.377-12-12 5.377-12 12-12zm0 1c6.071 0 11 4.929 11 11s-4.929 11-11 11-11-4.929-11-11 4.929-11 11-11zm.5 17h-1v-9h1v9zm-.5-12c.466 0 .845.378.845.845 0 .466-.379.844-.845.844-.466 0-.845-.378-.845-.844 0-.467.379-.845.845-.845z" />
 </IconBase>
+
+export const ArrowIcon: FC<IconBaseProps> = props => <IconBase
+  {...props}
+  svgProps={{ viewBox: "0 0 24 24" }}
+>
+  <path d="M3 12l18-12v24z" />
+</IconBase>
