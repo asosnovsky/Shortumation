8adb86b2ec68eeca35e69b5ef817c9286d675b4f
Ari Sosnovsky <Ariel@Sosnovsky.ca>
2021-12-30 23:15:41 -0500

    added support for ruamel types

--- api/src/json_serializer.py
+++ api/src/json_serializer.py
@@ -1,5 +1,10 @@
 import orjson
 from typing import Any, Union
+from ruamel.yaml.scalarstring import ScalarString
+from ruamel.yaml.scalarint import ScalarInt
+from ruamel.yaml.scalarbool import ScalarBoolean
+from ruamel.yaml.scalarfloat import ScalarFloat
+from src.yaml_serializer.types import SecretValue
 from src.yaml_serializer.types import SecretValue
 
 # TODO: add support for these
@@ -9,6 +14,14 @@ NOT_IMPLEMENTED_SV_MSG = "SECRET VALUE NOT IMPLEMENTED"
 def __default(obj: Any) -> str:
     if isinstance(obj, SecretValue):
         return NOT_IMPLEMENTED_SV_MSG
+    if isinstance(obj, ScalarString):
+        return str(obj)
+    if isinstance(obj, ScalarBoolean):
+        return bool(obj)
+    if isinstance(obj, ScalarFloat):
+        return float(obj)
+    if isinstance(obj, ScalarInt):
+        return int(obj)
     raise TypeError
 
 
--- api/tests/unittests/test_json_serializer.py
+++ api/tests/unittests/test_json_serializer.py
@@ -1,5 +1,9 @@
 import json
 from unittest import TestCase
+from ruamel.yaml.scalarstring import ScalarString
+from ruamel.yaml.scalarint import ScalarInt
+from ruamel.yaml.scalarbool import ScalarBoolean
+from ruamel.yaml.scalarfloat import ScalarFloat
 from src.yaml_serializer.types import SecretValue
 from src.json_serializer import json_dumps, NOT_IMPLEMENTED_SV_MSG
 
@@ -25,6 +29,29 @@ class json_serializer_tests(TestCase):
                     "list": [1, "w"],
                     "tup": (1, "w"),
                     "some": {"deep": {"obj": NOT_IMPLEMENTED_SV_MSG}},
+                },
+                separators=(",", ":"),
+            ),
+        )
+
+    def test_convert_ruamel_types(self):
+        self.assertEqual(
+            json_dumps(
+                {
+                    "int": ScalarInt(1),
+                    "float": ScalarFloat(1.3),
+                    "bool": ScalarBoolean(False),
+                    "list": [1, ScalarString("w")],
+                    "tup": (1, ScalarString("w")),
+                }
+            ),
+            json.dumps(
+                {
+                    "int": 1,
+                    "float": 1.3,
+                    "bool": 0,
+                    "list": [1, "w"],
+                    "tup": (1, "w"),
                 },
                 separators=(",", ":"),
             ),
