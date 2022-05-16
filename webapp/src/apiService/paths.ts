4e19bdf4c4d537f7acce30c57e4c8619fffc77d3
Ari <ariel@sosnovsky.ca>
2022-05-16 11:22:32 -0400

    changed up paths

--- webapp/src/apiService/paths.ts
+++ webapp/src/apiService/paths.ts
@@ -1,4 +1,4 @@
 
 
-export const AUTOMTAION_ROOT = '/' as const;
-export const AUTOMTAION_LIST = `${AUTOMTAION_ROOT}list` as const;
+export const AUTOMTAION_ROOT = '' as const;
+export const AUTOMTAION_LIST = `${AUTOMTAION_ROOT}/list` as const;
