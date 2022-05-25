f8233dd6ab8d4132fdf931cfac90f8767c14eb15
Ari <ariel@sosnovsky.ca>
2022-05-24 21:11:49 -0400

    added mui

--- webapp/.storybook/main.js
+++ webapp/.storybook/main.js
@@ -8,5 +8,8 @@ module.exports = {
         "@storybook/addon-essentials",
         "@storybook/preset-create-react-app",
         "@storybook/addon-queryparams",
-    ]
+    ],
+    "features": {
+        "emotionAlias": false,
+    },
 }
