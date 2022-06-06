78494ee74d786c51d0b15b86c38c56e3545e67e2
Ari <ariel@sosnovsky.ca>
2022-06-06 19:40:20 -0400

    fixed up some styling issues

--- webapp/src/apiService/index.ts
+++ webapp/src/apiService/index.ts
@@ -7,7 +7,7 @@ import { useMockAPI } from "./mock";
 const locationPrefixWeb = window.location.pathname.match(/(\/.+\/)web/i);
 const baseURL = new URL(
   process.env.NODE_ENV === 'development' ?
-    "http://localhost:8000" :
+    `http://${window.location.hostname}:8000` :
     window.location.origin + (!locationPrefixWeb ? '' : locationPrefixWeb[1])
 );
 export const wsURL = baseURL + "socket"
