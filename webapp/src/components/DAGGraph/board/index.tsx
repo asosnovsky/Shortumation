b4f46bec921bc7cbc348b0caad49e9480a5e4310
Ari <ariel@sosnovsky.ca>
2022-07-12 20:31:00 -0400

    zoom groups

--- webapp/src/components/DAGGraph/board/index.tsx
+++ webapp/src/components/DAGGraph/board/index.tsx
@@ -58,7 +58,7 @@ export const DAGGraphBoardInner: FC<DAGGraphBoardProps> = ({
     );
   }
   // this hack avoids breakage on when zooming
-  const prefix = Math.floor(zoom / (2.5 / 3));
+  const prefix = Math.floor(zoom / (2.5 / 6));
   const elements = {
     nodes: state.data.elements.nodes.map((n) => ({
       ...n,
